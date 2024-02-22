3443d81ea731d12dbac7ca072b8925b3fbc87fd2
Roisnel <roy.adan.trenal@gmail.com>
2024-02-22 11:19:27 -0500

    feat: :zap: Add functions and refactor names
    Function Insert Score and Get All Greter Than to supabase file. Refactor Table and column name to const.

--- src/helpers/supabase-queries.ts
+++ src/helpers/supabase-queries.ts
@@ -1,18 +1,35 @@
 import { supabase } from "./supabase-client";
 
+const TABLE_NAME = "ranks";
+const COLUMN_SCORE = "score";
 export const getAllRecords = async () => {
   const { data, error } = await supabase
-    .from("rank")
+    .from(TABLE_NAME)
     .select("*")
-    .order("points", { ascending: false });
+    .order(COLUMN_SCORE, { ascending: false });
 
   return { data, error };
 };
 export const getMaxScore = async () => {
   const { data, error } = await supabase
-    .from("rank")
+    .from(TABLE_NAME)
     .select("*")
-    .order("points", { ascending: false })
+    .order(COLUMN_SCORE, { ascending: false })
     .limit(1);
+  return { data, error };
+};
+
+export const insertScore = async (name: string, score: number) => {
+  const { error } = await supabase.from(TABLE_NAME).insert({ name, score });
+
+  return error;
+};
+
+export const getAllGreaterThan = async (value: number) => {
+  const { data, error } = await supabase
+    .from(TABLE_NAME)
+    .select()
+    .gt(COLUMN_SCORE, value);
+
   return { data, error };
 };
